a9efd89cc9ded2b1f2e1e3122f632280a2e07d5f
Giuliana Bentancor <giulianabentancor@gmail.com>
2025-05-07 15:01:34 -0300

    first commit

---
+++ src/main/java/base/ApiClient.java
@@ -0,0 +1,17 @@
+package base;
+
+public class ApiClient extends ApiClientBase {
+    private static ApiClient instance;
+
+    private ApiClient() {
+        super();
+    }
+
+    public static ApiClient getInstace() {
+        if (instance == null) {
+            instance = new ApiClient();
+        }
+
+        return instance;
+    }
+}
---
+++ src/main/java/models/requests/CredentialModel.java
@@ -0,0 +1,19 @@
+package models.requests;
+
+public class CredentialModel {
+    private String username;
+    private String password;
+
+    public CredentialModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
